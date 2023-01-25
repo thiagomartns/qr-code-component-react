af7a18af2fa555dc6d9ba2454db00dd933ded169
thiago <tmg3.mn@gmail.com>
2023-01-25 11:02:48 -0300

    Inserção de elementos HTML e de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      
+      <div className="appContainer">
+        <div className="content">
+          <div className="imgContainer">
+            <img src="/assets/img/image-qr-code.png" alt="" />
+          </div>
+          <div className="text">
+            <h2>Improve your front-end skills by building projects</h2>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
